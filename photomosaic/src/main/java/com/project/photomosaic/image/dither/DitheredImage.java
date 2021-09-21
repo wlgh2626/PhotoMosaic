37585bee1b9fc9bbd7c29f8d29bb061afb82ae3c
wlgh2626 <wlgh2626@hotmail.com>
2021-09-21 16:31:50 -0700

    Added testing for dithering image. Properly dithers image now

--- photomosaic/src/main/java/com/project/photomosaic/image/dither/DitheredImage.java
+++ photomosaic/src/main/java/com/project/photomosaic/image/dither/DitheredImage.java
@@ -5,28 +5,39 @@ import java.awt.image.BufferedImage;
 public class DitheredImage {
 	public static final int DEFAULT_DITHER_SIZE = 16;
 	private BufferedImage original;
+	private BufferedImage dithered;
 	private long[][] data;
 	private int size;	//original image gets sectioned to (size x size)
 
 	public DitheredImage(BufferedImage image) {
+		original = image;
 		size = DEFAULT_DITHER_SIZE;
-		build(image);
+		build();
 	}
 
 	public DitheredImage(BufferedImage image, int ditherSize) {
+		original = image;
 		this.size = ditherSize;
-		build(image);
+		build();
 	}
 
-	public void build(BufferedImage image) {
-		original = image;
+	private void build() {
 		int length = (int) Math.ceil((double) original.getWidth() / size);
 		int height = (int) Math.ceil((double) original.getHeight() / size);
 		data = new long[height][length];
 
 		for(int y = 0 ; y < original.getHeight() ; y++) {
 			for(int x = 0 ; x < original.getWidth() ; x++) {
-				data[y % height][x % length] += original.getRGB(x, y) & 0xFFFFFF;
+				int RGB = (original.getRGB(x, y) & 0xFFFFFF);
+				data[y/size][x/size] += RGB / (size*size);	//NOT CORRECT
+			}
+		}
+		
+		dithered = new BufferedImage(length*size , height*size ,  BufferedImage.TYPE_INT_RGB);
+		for(int y = 0 ; y < dithered.getHeight() ; y++) {
+			for(int x = 0 ; x < dithered.getWidth() ; x++){
+				int RGB = (int) data[y/size][x/size];
+				dithered.setRGB( x, y, RGB);
 			}
 		}
 	}
@@ -44,11 +55,25 @@ public class DitheredImage {
 	}
 
 	public int getRGB(int x, int y) {
-		return (int)( data[y][x] / (size*size));
+		return (int) data[y][x];
 	}
 
 	public BufferedImage getOriginalImage() {
 		return original;
+	}
+	
+	//returns image with SizeXSize block average pixel of that section
+	public BufferedImage getDitheredImage() {
+		return dithered;
+	}
+	
+	public String toString() {
+		return "Original Image Information: " + original + "\n" +
+			   "Number of Horizontal Dither Matrix (Length): " + data.length + "\n" +
+			   "Number of Vertical Dither Matrix (Height): " + data[0].length + "\n"+ 
+			   "Dithered Image Number of Pixels: " + data.length*data[0].length + "\n"+
+			   "Dithering Size: " + size + "\n";
+				
 	}
 
 }
--- photomosaic/src/test/java/com/project/photomosaic/DitheringTest.java
+++ photomosaic/src/test/java/com/project/photomosaic/DitheringTest.java
@@ -3,9 +3,12 @@ package com.project.photomosaic;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 
 import com.project.photomosaic.image.Photomosaic;
+import com.project.photomosaic.image.dither.DitheredImage;
 import com.project.photomosaic.image.sample.SampleIO;
 
 import javafx.application.Application;
@@ -18,14 +21,15 @@ public class DitheringTest extends Application {
 	private File original = new File(Photomosaic.ORIGINAL_DEFAULT_PATH + "/test/" + ImageDisplay.ORIGINAL_TEST_FILES.get(0));
 
 	@Test
-	public void balloonsIFDTest() throws Exception {
+	public void DitheringTest() throws Exception {
 		launch();
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Photomosaic photomosaic = new Photomosaic(original, sample);
-		ImageDisplay display = new ImageDisplay(photomosaic.getImage());
+		DitheredImage dithered = new DitheredImage(ImageIO.read(original));
+		System.out.println(dithered.toString());
+		ImageDisplay display = new ImageDisplay(dithered.getDitheredImage());
 
 		HBox hbox = new HBox(display.getImageView());
 		Scene scene = new Scene(hbox);
