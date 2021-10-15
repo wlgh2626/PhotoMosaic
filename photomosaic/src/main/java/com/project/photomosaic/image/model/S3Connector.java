addaa9276fa45a21f0d6270b8060b8093fd81ebe
wlgh2626 <wlgh2626@hotmail.com>
2021-10-15 13:19:27 -0700

    changed reading auth key as JSON file

--- photomosaic/src/main/java/com/project/photomosaic/image/model/S3Connector.java
+++ photomosaic/src/main/java/com/project/photomosaic/image/model/S3Connector.java
@@ -5,30 +5,30 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.*;
 import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Logger;
-
 
 public class S3Connector {
 	private static final Logger logger = Logger.getLogger(S3Connector.class.getName());
+	private static final String AUTH_PATH = System.getProperty("user.dir") + "/apiKey/s3auth.js";
 	
-	private String bucketName = "js-image-storage";
-	private String accessKey , secretKey;
+	private final String bucketName, accessKey , secretKey;
 	private Regions region = Regions.US_EAST_2;
 	
+	private AmazonS3 s3;
 	public S3Connector(String folderName) throws IOException{
-		retrieve();
+		JsonObject json = new Gson().fromJson( new FileReader(AUTH_PATH) , JsonObject.class);
+		bucketName = json.get("bucketName").getAsString();
+		accessKey = json.get("accessKey").getAsString();
+		secretKey = json.get("secretKey").getAsString();
+		
 		BasicAWSCredentials credential = new BasicAWSCredentials(accessKey, secretKey);
-		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+		s3 = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credential))
                 .withRegion(region)
                 .build();
@@ -36,12 +36,7 @@ public class S3Connector {
 		for(S3ObjectSummary summary : s3Objects.getObjectSummaries()) {
 			System.out.println(summary.getKey());
 		}
-	}
-	
-	public void retrieve() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("apiKey/s3auth.txt"));
-		accessKey = reader.readLine();
-		secretKey = reader.readLine();
+		
 	}
 	
 }
