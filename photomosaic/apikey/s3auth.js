4d3435cb48b1b2458f2280ce3698e40f97463113
wlgh2626 <wlgh2626@hotmail.com>
2021-10-20 00:21:10 -0700

    Update s3auth.js

--- photomosaic/apikey/s3auth.js
+++ photomosaic/apikey/s3auth.js
@@ -1,5 +1,5 @@
 {
     "bucketName":"js-image-storage",
-    "accessKey":"AKIAZR6PUDYLRBRAX4MW",
-    "secretKey":"7HPc46DKjwtxtj5+J4tG2J/Sih99EdtQaTsUXmKe"
+    "accessKey":"your-access-key-here",
+    "secretKey":"your-secret-key-here"
 }
