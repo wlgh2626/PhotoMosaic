e318b49a37cb1585e59727d548cec3b416c616c6
wlgh2626 <wlgh2626@hotmail.com>
2021-10-28 16:30:13 -0700

    cors enabled

--- backend/s3connector/server.js
+++ backend/s3connector/server.js
@@ -1,14 +1,15 @@
 import express from "express"
+import cors from "cors"
 import { S3ConnectionURL } from './s3Connector.js'
 
+const portNumber = process.env.SERVER_PORT
 const app = express();
-
-app.use(express.static('front'));
+app.use(cors());
 
 app.get('/requestS3Url', async (req, res) => {
   const data = await S3ConnectionURL(req.query.key);
   console.log(data);
-  res.send({data});
+  res.send(data);
 })
 
-app.listen(8080, () => console.log("listening on port 8080"));
+app.listen(portNumber, () => console.log("listening on port " + portNumber) );
