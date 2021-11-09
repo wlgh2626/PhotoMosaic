ab5f18e4c7db7a82ca05d997fbd9c66f82cd148c
wlgh2626 <wlgh2626@hotmail.com>
2021-11-09 12:58:27 -0800

    added allowed origin in cors

--- backend/s3connector/server.js
+++ backend/s3connector/server.js
@@ -7,7 +7,7 @@ import { S3ConnectionURL } from './s3Connector.js'
 const portNumber = process.env.SERVER_PORT
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: true}));
 app.get('/requestS3Url', async (req, res) => {
   const data = await S3ConnectionURL(req.query.key);
   console.log(data);
