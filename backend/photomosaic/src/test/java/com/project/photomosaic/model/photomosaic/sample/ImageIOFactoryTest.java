4dcf7146f428459d2731b94ed4f646b0e6c21171
wlgh2626 <wlgh2626@hotmail.com>
2021-11-23 17:33:47 -0800

    Test now compares between single core vs multi core

--- backend/photomosaic/src/test/java/com/project/photomosaic/model/photomosaic/sample/ImageIOFactoryTest.java
+++ backend/photomosaic/src/test/java/com/project/photomosaic/model/photomosaic/sample/ImageIOFactoryTest.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +26,19 @@ public class ImageIOFactoryTest {
 
 	@Test
 	public void ParallelImageIOTest() throws InterruptedException {
-		System.out.println(Runtime.getRuntime().availableProcessors());
-		ImageIOFactory factory = new ImageIOFactory(testFiles);
-		ArrayList<BufferedImage> images = factory.getImages();
+		System.out.println("There are " + Runtime.getRuntime().availableProcessors() + " available cores");
+		ImageIOFactory factory1 = new ImageIOFactory();
+		long start1 = System.currentTimeMillis();
+		ArrayList<BufferedImage> images1 = factory1.asBufferedImages(testFiles);
+		long end1 = System.currentTimeMillis();
+		long singleCoreTime = end1 - start1;
+
+		ImageIOFactory factory2 = new ImageIOFactory(Runtime.getRuntime().availableProcessors());
+		long start2 = System.currentTimeMillis();
+		ArrayList<BufferedImage> images2 = factory2.asBufferedImages(testFiles);
+		long end2 = System.currentTimeMillis();
+		long multiCoreTime = end2 - start2;
+
+		Assert.assertTrue(multiCoreTime < singleCoreTime);
 	}
 }
