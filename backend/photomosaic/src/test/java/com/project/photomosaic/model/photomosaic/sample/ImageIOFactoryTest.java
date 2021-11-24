8852c544fe4d644b694db1dd2b2b638ac44cb930
wlgh2626 <wlgh2626@hotmail.com>
2021-11-23 18:19:35 -0800

    now sends list of byte array, instead of file list

--- backend/photomosaic/src/test/java/com/project/photomosaic/model/photomosaic/sample/ImageIOFactoryTest.java
+++ backend/photomosaic/src/test/java/com/project/photomosaic/model/photomosaic/sample/ImageIOFactoryTest.java
@@ -2,8 +2,10 @@ package com.project.photomosaic.model.photomosaic.sample;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,20 +34,24 @@ public class ImageIOFactoryTest {
 	ArrayList<File> testFiles;
 
 	@Test
-	public void ParallelImageIOTest() throws InterruptedException {
+	public void ParallelImageIOTest() throws InterruptedException, IOException {
+		ArrayList<byte[]> fileBytesList = new ArrayList<byte[]>();
+		for (File file : testFiles) {
+			fileBytesList.add(FileUtils.readFileToByteArray(file));
+		}
 		int numThreads = Runtime.getRuntime().availableProcessors();
 		System.out.println("There are " + numThreads + " available cores");
 
 		ImageIOFactory factory1 = new ImageIOFactory(1); // 1 core only
 		long start1 = System.currentTimeMillis();
-		ArrayList<BufferedImage> images1 = factory1.asBufferedImages(testFiles);
+		ArrayList<BufferedImage> images1 = factory1.asBufferedImages(fileBytesList);
 		long end1 = System.currentTimeMillis();
 		long singleCoreTime = end1 - start1;
 		System.out.println("Time for single core: " + singleCoreTime / 1000 + "seconds");
 
 		ImageIOFactory factory2 = new ImageIOFactory(numThreads); // all cores
 		long start2 = System.currentTimeMillis();
-		ArrayList<BufferedImage> images2 = factory2.asBufferedImages(testFiles);
+		ArrayList<BufferedImage> images2 = factory2.asBufferedImages(fileBytesList);
 		long end2 = System.currentTimeMillis();
 		long multiCoreTime = end2 - start2;
 		System.out.println("Time for " + numThreads + " cores: " + multiCoreTime / 1000 + "seconds");
